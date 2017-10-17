0675ed12663ccf641795ce864fdc398c2f4913ee
Christoph Frick <cf@ofnir.net>
2017-10-17 12:21:39 +0200

    Enable HTTP/2

--- src/main/groovy/app/Application.groovy
+++ src/main/groovy/app/Application.groovy
@@ -8,9 +8,14 @@ import com.vaadin.ui.html.Div
 import com.vaadin.ui.html.H1
 import com.vaadin.ui.textfield.TextField
 import groovy.util.logging.Slf4j
+import org.apache.coyote.http2.Http2Protocol
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory
 import org.springframework.boot.web.support.SpringBootServletInitializer
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 class Application extends SpringBootServletInitializer {
@@ -18,6 +23,18 @@ class Application extends SpringBootServletInitializer {
         def app = new SpringApplication(Application)
         // further config...
         app.run(args)
+    }
+
+    // enable http2
+    @Bean
+    EmbeddedServletContainerCustomizer tomcatCustomizer() {
+        { container ->
+            if (container instanceof TomcatEmbeddedServletContainerFactory) {
+                (container as TomcatEmbeddedServletContainerFactory).addConnectorCustomizers({ connector ->
+                    connector.addUpgradeProtocol(new Http2Protocol())
+                } as TomcatConnectorCustomizer)
+            }
+        }
     }
 }
 
