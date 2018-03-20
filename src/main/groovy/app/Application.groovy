1ac81887a887afc5db3a107464d5e3f5a7db825d
Christoph Frick <cf@ofnir.net>
2018-03-20 14:54:07 +0100

    RouterLayout and navigation between views

--- src/main/groovy/app/Application.groovy
+++ src/main/groovy/app/Application.groovy
@@ -1,16 +1,21 @@
 package app
 
-import com.vaadin.flow.component.Composite
-import com.vaadin.flow.component.HasValue
+import com.vaadin.flow.component.Component
 import com.vaadin.flow.component.button.Button
 import com.vaadin.flow.component.dependency.HtmlImport
+import com.vaadin.flow.component.formlayout.FormLayout
 import com.vaadin.flow.component.html.Div
-import com.vaadin.flow.component.html.H1
 import com.vaadin.flow.component.page.BodySize
 import com.vaadin.flow.component.page.Viewport
 import com.vaadin.flow.component.textfield.TextField
-import com.vaadin.flow.data.value.ValueChangeMode
+import com.vaadin.flow.data.binder.Binder
+import com.vaadin.flow.router.BeforeEvent
+import com.vaadin.flow.router.HasUrlParameter
+import com.vaadin.flow.router.OptionalParameter
+import com.vaadin.flow.router.ParentLayout
 import com.vaadin.flow.router.Route
+import com.vaadin.flow.router.RouterLayout
+import com.vaadin.flow.router.RouterLink
 import com.vaadin.flow.theme.Theme
 import com.vaadin.flow.theme.lumo.Lumo
 import groovy.util.logging.Slf4j
@@ -27,31 +32,58 @@ class Application extends SpringBootServletInitializer {
     }
 }
 
-@Route('')
 @Slf4j
 @Theme(Lumo)
 @HtmlImport('frontend:///styles.html')
 @BodySize(height = "100vh", width = "100vw")
 @Viewport("width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes")
-class MainLayout extends Composite<Div> {
-    MainLayout() {
-        H1 label
-        TextField input
-        content.add(
-                label = new H1(),
-                input = new TextField().with {
-                    addValueChangeListener({
-                        label.text = "Hello ${it.value ?: "World"}"
-                    } as HasValue.ValueChangeListener)
-                    valueChangeMode = ValueChangeMode.EAGER
-                    value = "Flow"
-                    focus()
-                    it
-                },
-                new Button("Greet the server log", {
-                    log.info "Hello ${input.value ?: "World"} to the server log"
-                }),
+class MainLayout extends Div implements RouterLayout {
+}
+
+@ParentLayout(MainLayout)
+class MenuLayout extends Div implements RouterLayout {
+    MenuLayout() {
+        addClassName('menu')
+        addMenuItem("Dashboard", DashboardView)
+        addMenuItem("Hello", HelloView)
+    }
+    def addMenuItem(String label, Class<? extends Component> viewClass) {
+        add(new RouterLink(label, viewClass))
+    }
+}
+
+class HelloRequest {
+    String text
+}
+
+@Route(value="", layout = MenuLayout)
+class DashboardView extends Div {
+    DashboardView() {
+        def input = new TextField("Say hello to")
+        def binder = new Binder<HelloRequest>(HelloRequest)
+        binder
+            .forField(input)
+            .bind("text")
+        binder.bean = new HelloRequest()
+        def button = new Button("Go", {
+            getUI().ifPresent{ it.navigate("hello/${binder.bean.text}") }
+        })
+        add(
+                new FormLayout(
+                        input,
+                        button
+                )
         )
     }
 }
 
+@Route(value="hello", layout = MenuLayout)
+class HelloView extends Div implements HasUrlParameter<String> {
+    HelloView() {
+        text = "Dashboard"
+    }
+    @Override
+    void setParameter(BeforeEvent event, @OptionalParameter String parameter) {
+        setText("Hello, ${parameter?:"World"}!")
+    }
+}
