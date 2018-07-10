06a7ee6978d5220decc4c990abefc59300c00a9a
Christoph Frick <cf@ofnir.net>
2018-07-10 14:46:08 +0200

    Add a grid with variable row height depending on content

--- src/main/groovy/app/Application.groovy
+++ src/main/groovy/app/Application.groovy
@@ -1,24 +1,29 @@
 package app
 
-import com.vaadin.flow.component.Composite
-import com.vaadin.flow.component.HasValue
-import com.vaadin.flow.component.button.Button
+import com.devskiller.jfairy.Fairy
 import com.vaadin.flow.component.dependency.HtmlImport
-import com.vaadin.flow.component.html.Div
-import com.vaadin.flow.component.html.H1
+import com.vaadin.flow.component.grid.Grid
+import com.vaadin.flow.component.icon.Icon
+import com.vaadin.flow.component.icon.VaadinIcon
+import com.vaadin.flow.component.orderedlayout.VerticalLayout
 import com.vaadin.flow.component.page.BodySize
 import com.vaadin.flow.component.page.Viewport
-import com.vaadin.flow.component.textfield.TextField
-import com.vaadin.flow.data.value.ValueChangeMode
+import com.vaadin.flow.data.provider.AbstractBackEndDataProvider
+import com.vaadin.flow.data.provider.Query
+import com.vaadin.flow.data.renderer.ComponentRenderer
+import com.vaadin.flow.data.renderer.TemplateRenderer
+import com.vaadin.flow.function.SerializableFunction
 import com.vaadin.flow.router.Route
 import com.vaadin.flow.theme.Theme
 import com.vaadin.flow.theme.lumo.Lumo
+import groovy.transform.Canonical
 import groovy.util.logging.Slf4j
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+
+import java.time.LocalDate
+import java.util.stream.Stream
 
 @SpringBootApplication
 class Application extends SpringBootServletInitializer {
@@ -29,38 +34,77 @@ class Application extends SpringBootServletInitializer {
     }
 }
 
-@RestController
-class HelloWorldController {
-    @GetMapping("/hello")
-    String index() {
-        return "Hello World"
-    }
-}
-
 @Route('')
 @Slf4j
 @Theme(Lumo)
 @HtmlImport('frontend:///styles.html')
 @BodySize(height = "100vh", width = "100vw")
 @Viewport("width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes")
-class MainLayout extends Composite<Div> {
+class MainLayout extends VerticalLayout {
     MainLayout() {
-        H1 label
-        TextField input
-        content.add(
-                label = new H1(),
-                input = new TextField().tap {
-                    addValueChangeListener({
-                        label.text = "Hello ${it.value ?: "World"}"
-                    } as HasValue.ValueChangeListener)
-                    valueChangeMode = ValueChangeMode.EAGER
-                    value = "Flow"
-                    focus()
-                },
-                new Button("Greet the server log", {
-                    log.info "Hello ${input.value ?: "World"} to the server log"
-                }),
+        def grid = new Grid<Item>()
+        grid.setDataProvider(
+                new AbstractBackEndDataProvider<Item, Object>() {
+                    @Override
+                    protected Stream<Item> fetchFromBackEnd(Query<Item, Object> query) {
+                        return Item.TEST_DATA.stream().skip(query.offset).limit(query.limit)
+                    }
+
+                    @Override
+                    protected int sizeInBackEnd(Query<Item, Object> query) {
+                        return Item.TEST_DATA.size()
+                    }
+
+                    @Override
+                    boolean isInMemory() {
+                        false // BS
+                    }
+                }
         )
+        grid.addColumn(new ComponentRenderer<Icon, Item>({
+            it?.type?.buildComponent() ?: new Icon(VaadinIcon.COG)
+        } as SerializableFunction<Item, Icon>)).setHeader("Type").setSortable(true).setFlexGrow(0)
+        grid.addColumn(
+                TemplateRenderer.<Item> of("""<strong>[[item.headline]]</strong><br /><small>[[item.preface]]</small>""")
+                        .withProperty('headline', { it.headline })
+                        .withProperty('preface', { it.preface })
+        ).setHeader("Abstract").setSortable(true)
+        grid.addColumn({ it.publication }).setHeader("Publication Date").setSortable(true).setFlexGrow(0)
+        grid.setSizeFull()
+        grid.element.themeList.addAll('wrap-cell-content', 'row-stripes')
+        add(grid)
+        setWidth('40rem')
+        setHeight("100%")
     }
 }
 
+@Canonical
+class Item {
+    Type type
+    String headline
+    String preface
+    LocalDate publication
+
+    static enum Type {
+        ARTICLE(VaadinIcon.PENCIL), BOOK(VaadinIcon.BOOK)
+        final VaadinIcon icon
+
+        Type(VaadinIcon icon) {
+            this.icon = icon
+        }
+
+        Icon buildComponent() {
+            new Icon(icon)
+        }
+    }
+
+    private static final Fairy data = Fairy.create(Locale.ENGLISH)
+    public static final List<Item> TEST_DATA = (0..100).collect {
+        new Item(
+                Type.ARTICLE,
+                data.textProducer().latinSentence(),
+                data.textProducer().paragraph(),
+                data.dateProducer().randomDateInThePast(10).toLocalDate()
+        )
+    }
+}
