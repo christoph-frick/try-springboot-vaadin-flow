64e5e09052d1c40744e1b2ac73b16038ebe3d13f
Christoph Frick <cf@ofnir.net>
2018-11-26 15:41:53 +0100

    Tag select component

--- src/main/groovy/app/Application.groovy
+++ src/main/groovy/app/Application.groovy
@@ -1,18 +1,31 @@
 package app
 
+import com.sun.xml.internal.bind.v2.TODO
+import com.vaadin.flow.component.AttachEvent
 import com.vaadin.flow.component.Composite
-import com.vaadin.flow.component.HasValue
-import com.vaadin.flow.component.button.Button
+import com.vaadin.flow.component.DetachEvent
+import com.vaadin.flow.component.checkbox.Checkbox
+import com.vaadin.flow.component.combobox.ComboBox
 import com.vaadin.flow.component.dependency.HtmlImport
 import com.vaadin.flow.component.html.Div
 import com.vaadin.flow.component.html.H1
 import com.vaadin.flow.component.page.BodySize
 import com.vaadin.flow.component.page.Viewport
-import com.vaadin.flow.component.textfield.TextField
-import com.vaadin.flow.data.value.ValueChangeMode
+import com.vaadin.flow.component.polymertemplate.EventHandler
+import com.vaadin.flow.component.polymertemplate.Id
+import com.vaadin.flow.component.polymertemplate.ModelItem
+import com.vaadin.flow.component.polymertemplate.PolymerTemplate
+import com.vaadin.flow.data.provider.DataProvider
+import com.vaadin.flow.data.provider.ListDataProvider
 import com.vaadin.flow.router.Route
+import com.vaadin.flow.shared.Registration
+import com.vaadin.flow.templatemodel.Include
+import com.vaadin.flow.templatemodel.TemplateModel
 import com.vaadin.flow.theme.Theme
 import com.vaadin.flow.theme.lumo.Lumo
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
+import groovy.transform.TupleConstructor
 import groovy.util.logging.Slf4j
 import org.springframework.boot.Banner
 import org.springframework.boot.SpringApplication
@@ -48,22 +61,95 @@ class HelloWorldController {
 @Viewport("width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes")
 class MainLayout extends Composite<Div> {
     MainLayout() {
-        H1 label
-        TextField input
+        def tagSelect = new TagSelectComponent().tap{
+            selected = [
+                    new TagSelectComponent.Tag("x", "X"),
+                    new TagSelectComponent.Tag("y", "Y"),
+            ].toSet()
+            dataProvider = ListDataProvider.ofCollection([
+                    new TagSelectComponent.Tag("x", "X"),
+                    new TagSelectComponent.Tag("y", "Y"),
+                    new TagSelectComponent.Tag("z", "Z"),
+            ])
+        }
         content.add(
-                label = new H1(),
-                input = new TextField().tap {
-                    addValueChangeListener({
-                        label.text = "Hello ${it.value ?: "World"}"
-                    } as HasValue.ValueChangeListener)
-                    valueChangeMode = ValueChangeMode.EAGER
-                    value = "Flow"
-                    focus()
-                },
-                new Button("Greet the server log", {
-                    log.info "Hello ${input.value ?: "World"} to the server log"
-                }),
+                new H1("Tag Select"),
+                new Div(
+                        new Checkbox("Horizontal?").tap {
+                            addValueChangeListener{
+                                tagSelect.element.themeList.set('horizontal', it.value)
+                            }
+                        },
+                ),
+                tagSelect
         )
     }
 }
 
+
+@com.vaadin.flow.component.Tag('tag-select')
+@HtmlImport("tag-select.html")
+class TagSelectComponent extends PolymerTemplate<TagSelectModel> {
+
+    @Id('select')
+    ComboBox<Tag> select
+
+    private Registration registration
+
+    @TupleConstructor
+    @ToString
+    @EqualsAndHashCode(includes = ['id'])
+    static class Tag {
+        String id
+        String label
+    }
+
+    static interface TagSelectModel extends TemplateModel {
+        @Include(["id","label"])
+        void setSelected(List<Tag> tags)
+        List<Tag> getSelected()
+    }
+
+    @EventHandler
+    void deselect(@ModelItem Tag tag) {
+        selected = selected.findAll{ it.id!=tag.id }
+    }
+
+    @Override
+    protected void onAttach(AttachEvent attachEvent) {
+        super.onAttach(attachEvent)
+        select.tap {
+            itemLabelGenerator = { it.label }
+            registration = addValueChangeListener {
+                if (it.value) {
+                    selected = selected + [it.value]
+                    value = null // FIXME: triggers value change again and seems to roundtrip
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onDetach(DetachEvent detachEvent) {
+        super.onDetach(detachEvent)
+        if (registration) {
+            registration.remove()
+            registration = null
+        }
+    }
+
+    // TODO: can we delegate to a super class method?
+
+    void setSelected(Set<Tag> tags) {
+        model.setSelected(tags.toList())
+    }
+
+    Set<Tag> getSelected() {
+        model.selected.toSet()
+    }
+
+    void setDataProvider(DataProvider<Tag, String> dataProvider) {
+        select.dataProvider = dataProvider
+    }
+
+}
