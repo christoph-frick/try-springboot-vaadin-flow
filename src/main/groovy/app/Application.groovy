ff1c512dab3746117d6db643338e8ef1d4e330cf
Christoph Frick <cf@ofnir.net>
2019-02-05 12:11:58 +0100

    Toggle theme between light/dark; change color in light/dark

--- src/main/groovy/app/Application.groovy
+++ src/main/groovy/app/Application.groovy
@@ -1,15 +1,11 @@
 package app
 
 import com.vaadin.flow.component.Composite
-import com.vaadin.flow.component.HasValue
-import com.vaadin.flow.component.button.Button
+import com.vaadin.flow.component.checkbox.Checkbox
 import com.vaadin.flow.component.dependency.HtmlImport
 import com.vaadin.flow.component.html.Div
-import com.vaadin.flow.component.html.H1
 import com.vaadin.flow.component.page.BodySize
 import com.vaadin.flow.component.page.Viewport
-import com.vaadin.flow.component.textfield.TextField
-import com.vaadin.flow.data.value.ValueChangeMode
 import com.vaadin.flow.router.Route
 import com.vaadin.flow.theme.Theme
 import com.vaadin.flow.theme.lumo.Lumo
@@ -24,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController
 @SpringBootApplication
 class Application extends SpringBootServletInitializer {
     static void main(String[] args) throws Exception {
-        new SpringApplication(Application).tap{
+        new SpringApplication(Application).tap {
             // further config...
             bannerMode = Banner.Mode.OFF
             run(args)
@@ -48,21 +44,19 @@ class HelloWorldController {
 @Viewport("width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes")
 class MainLayout extends Composite<Div> {
     MainLayout() {
-        H1 label
-        TextField input
         content.add(
-                label = new H1(),
-                input = new TextField().tap {
-                    addValueChangeListener({
-                        label.text = "Hello ${it.value ?: "World"}"
-                    } as HasValue.ValueChangeListener)
-                    valueChangeMode = ValueChangeMode.EAGER
-                    value = "Flow"
-                    focus()
-                },
-                new Button("Greet the server log", {
-                    log.info "Hello ${input.value ?: "World"} to the server log"
-                }),
+                new Checkbox("Use Dark Theme").tap {
+                    addValueChangeListener { cb ->
+                        getUI().ifPresent { ui ->
+                            def themeList = ui.getElement().getThemeList()
+                            if (cb.value) {
+                                themeList.add(Lumo.DARK)
+                            } else {
+                                themeList.remove(Lumo.DARK)
+                            }
+                        }
+                    }
+                }
         )
     }
 }
