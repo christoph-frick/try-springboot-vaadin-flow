1791607a4463977883eda9559f47ccbaef30b977
Christoph Frick <cf@ofnir.net>
2019-02-05 14:18:13 +0100

    Add vaadin-app-layout; style the navbar

--- src/main/groovy/app/Application.groovy
+++ src/main/groovy/app/Application.groovy
@@ -1,15 +1,18 @@
 package app
 
 import com.vaadin.flow.component.Composite
-import com.vaadin.flow.component.HasValue
-import com.vaadin.flow.component.button.Button
+import com.vaadin.flow.component.Html
+import com.vaadin.flow.component.applayout.AppLayout
+import com.vaadin.flow.component.combobox.ComboBox
 import com.vaadin.flow.component.dependency.HtmlImport
 import com.vaadin.flow.component.html.Div
 import com.vaadin.flow.component.html.H1
+import com.vaadin.flow.component.html.Span
+import com.vaadin.flow.component.icon.VaadinIcon
+import com.vaadin.flow.component.orderedlayout.VerticalLayout
 import com.vaadin.flow.component.page.BodySize
 import com.vaadin.flow.component.page.Viewport
 import com.vaadin.flow.component.textfield.TextField
-import com.vaadin.flow.data.value.ValueChangeMode
 import com.vaadin.flow.router.Route
 import com.vaadin.flow.theme.Theme
 import com.vaadin.flow.theme.lumo.Lumo
@@ -24,7 +27,7 @@ import org.springframework.web.bind.annotation.RestController
 @SpringBootApplication
 class Application extends SpringBootServletInitializer {
     static void main(String[] args) throws Exception {
-        new SpringApplication(Application).tap{
+        new SpringApplication(Application).tap {
             // further config...
             bannerMode = Banner.Mode.OFF
             run(args)
@@ -48,21 +51,35 @@ class HelloWorldController {
 @Viewport("width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes")
 class MainLayout extends Composite<Div> {
     MainLayout() {
-        H1 label
-        TextField input
         content.add(
-                label = new H1(),
-                input = new TextField().tap {
-                    addValueChangeListener({
-                        label.text = "Hello ${it.value ?: "World"}"
-                    } as HasValue.ValueChangeListener)
-                    valueChangeMode = ValueChangeMode.EAGER
-                    value = "Flow"
-                    focus()
-                },
-                new Button("Greet the server log", {
-                    log.info "Hello ${input.value ?: "World"} to the server log"
-                }),
+                new AppLayout().tap {
+                    branding = new Html("<span><strong>Branded</strong>&amp;<em>Exiled</em></span>")
+                    content = new VerticalLayout(
+                            new H1("Lorem Ipsum"),
+                            new Span("Lorem Ipsum"),
+                            new TextField("Some input")
+                    )
+                    createMenu().tap {
+                        addMenuItem(VaadinIcon.ABACUS.create(), "About")
+                        addMenuItem(VaadinIcon.COGS.create(), "Settings")
+                        addMenuItem(new ComboBox().tap{
+                            setItems([Lumo.LIGHT,Lumo.DARK])
+                            value = Lumo.LIGHT
+                            addValueChangeListener{ theme ->
+                                UI.ifPresent{ ui ->
+                                    def themeList = ui.element.themeList
+                                    if (theme.value==Lumo.LIGHT) {
+                                        themeList.remove(Lumo.DARK)
+                                        themeList.add(Lumo.LIGHT)
+                                    } else {
+                                        themeList.remove(Lumo.LIGHT)
+                                        themeList.add(Lumo.DARK)
+                                    }
+                                }
+                            }
+                        })
+                    }
+                }
         )
     }
 }
