ac3bad434ed1f5cda76545014e23c00995b9e6e0
Christoph Frick <cf@ofnir.net>
2019-03-08 14:19:46 +0100

    Error handling via RouteNotFoundError

--- src/main/groovy/app/Application.groovy
+++ src/main/groovy/app/Application.groovy
@@ -1,19 +1,14 @@
 package app
 
 import com.vaadin.flow.component.Composite
-import com.vaadin.flow.component.HasValue
 import com.vaadin.flow.component.button.Button
 import com.vaadin.flow.component.dependency.HtmlImport
-import com.vaadin.flow.component.html.Div
 import com.vaadin.flow.component.html.H1
 import com.vaadin.flow.component.orderedlayout.VerticalLayout
 import com.vaadin.flow.component.page.BodySize
 import com.vaadin.flow.component.page.Viewport
-import com.vaadin.flow.component.textfield.TextField
-import com.vaadin.flow.data.value.ValueChangeMode
-import com.vaadin.flow.router.Route
+import com.vaadin.flow.router.*
 import com.vaadin.flow.theme.Theme
-import com.vaadin.flow.theme.lumo.Lumo
 import com.vaadin.flow.theme.material.Material
 import groovy.util.logging.Slf4j
 import org.springframework.boot.Banner
@@ -23,10 +18,12 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+import javax.servlet.http.HttpServletResponse
+
 @SpringBootApplication
 class Application extends SpringBootServletInitializer {
     static void main(String[] args) throws Exception {
-        new SpringApplication(Application).tap{
+        new SpringApplication(Application).tap {
             // further config...
             bannerMode = Banner.Mode.OFF
             run(args)
@@ -50,22 +47,17 @@ class HelloWorldController {
 @Viewport("width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes")
 class MainLayout extends Composite<VerticalLayout> {
     MainLayout() {
-        H1 label
-        TextField input
         content.add(
-                label = new H1(),
-                input = new TextField("Enter name to greet...").tap {
-                    addValueChangeListener({
-                        label.text = "Hello ${it.value ?: "World"}"
-                    } as HasValue.ValueChangeListener)
-                    valueChangeMode = ValueChangeMode.EAGER
-                    value = "Flow"
-                    focus()
-                },
-                new Button("Greet the server log", {
-                    log.info "Hello ${input.value ?: "World"} to the server log"
-                }),
+                new H1("Works")
         )
     }
 }
 
+@Route('error')
+class ErrorLayout extends RouteNotFoundError {
+    @Override
+    int setErrorParameter(BeforeEnterEvent event, ErrorParameter<NotFoundException> parameter) {
+        element.setText("This target does not exist")
+        return HttpServletResponse.SC_NOT_FOUND
+    }
+}
