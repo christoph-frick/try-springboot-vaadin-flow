eeff0a6ce1b92e8f3dc8509427ca2f0836ebf79b
Christoph Frick <cf@ofnir.net>
2019-03-12 13:41:26 +0100

    Render enum as icon in a grid

--- src/main/groovy/app/Application.groovy
+++ src/main/groovy/app/Application.groovy
@@ -1,32 +1,29 @@
 package app
 
 import com.vaadin.flow.component.Composite
-import com.vaadin.flow.component.HasValue
-import com.vaadin.flow.component.button.Button
 import com.vaadin.flow.component.dependency.HtmlImport
-import com.vaadin.flow.component.html.Div
-import com.vaadin.flow.component.html.H1
+import com.vaadin.flow.component.grid.Grid
+import com.vaadin.flow.component.icon.Icon
+import com.vaadin.flow.component.icon.VaadinIcon
 import com.vaadin.flow.component.orderedlayout.VerticalLayout
 import com.vaadin.flow.component.page.BodySize
 import com.vaadin.flow.component.page.Viewport
-import com.vaadin.flow.component.textfield.TextField
-import com.vaadin.flow.data.value.ValueChangeMode
+import com.vaadin.flow.data.renderer.ComponentRenderer
+import com.vaadin.flow.function.SerializableFunction
 import com.vaadin.flow.router.Route
 import com.vaadin.flow.theme.Theme
-import com.vaadin.flow.theme.lumo.Lumo
 import com.vaadin.flow.theme.material.Material
+import groovy.transform.Immutable
 import groovy.util.logging.Slf4j
 import org.springframework.boot.Banner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class Application extends SpringBootServletInitializer {
     static void main(String[] args) throws Exception {
-        new SpringApplication(Application).tap{
+        new SpringApplication(Application).tap {
             // further config...
             bannerMode = Banner.Mode.OFF
             run(args)
@@ -34,12 +31,21 @@ class Application extends SpringBootServletInitializer {
     }
 }
 
-@RestController
-class HelloWorldController {
-    @GetMapping("/hello")
-    String index() {
-        return "Hello World"
+enum State {
+
+    YES(VaadinIcon.PLUS_CIRCLE_O), NO(VaadinIcon.MINUS_CIRCLE_O), MAYBE(VaadinIcon.QUESTION_CIRCLE_O)
+
+    VaadinIcon icon
+
+    State(VaadinIcon icon) {
+        this.icon = icon
     }
+}
+
+@Immutable
+class Bean {
+    String name
+    State state
 }
 
 @Route('')
@@ -50,22 +56,16 @@ class HelloWorldController {
 @Viewport("width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes")
 class MainLayout extends Composite<VerticalLayout> {
     MainLayout() {
-        H1 label
-        TextField input
-        content.add(
-                label = new H1(),
-                input = new TextField("Enter name to greet...").tap {
-                    addValueChangeListener({
-                        label.text = "Hello ${it.value ?: "World"}"
-                    } as HasValue.ValueChangeListener)
-                    valueChangeMode = ValueChangeMode.EAGER
-                    value = "Flow"
-                    focus()
-                },
-                new Button("Greet the server log", {
-                    log.info "Hello ${input.value ?: "World"} to the server log"
-                }),
-        )
+        content.add(new Grid<Bean>(Bean).tap {
+            removeAllColumns()
+            addColumn('name')
+            addColumn(new ComponentRenderer({ new Icon(it.state.icon) } as SerializableFunction)).setHeader("State")
+            setItems(
+                    new Bean("Foo", State.YES),
+                    new Bean("Bar", State.NO),
+                    new Bean("Qux", State.MAYBE),
+            )
+        })
     }
 }
 
