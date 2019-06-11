75703a5713b876398f17787bc3a7559190fd7a7a
Christoph Frick <cf@ofnir.net>
2019-06-11 15:31:38 +0200

    POC: declarative component placement

--- src/main/groovy/app/Application.groovy
+++ src/main/groovy/app/Application.groovy
@@ -1,32 +1,38 @@
 package app
 
-import com.vaadin.flow.component.Composite
-import com.vaadin.flow.component.HasValue
+import com.vaadin.flow.component.Component
 import com.vaadin.flow.component.button.Button
 import com.vaadin.flow.component.dependency.HtmlImport
 import com.vaadin.flow.component.html.Div
 import com.vaadin.flow.component.html.H1
-import com.vaadin.flow.component.orderedlayout.VerticalLayout
+import com.vaadin.flow.component.html.Span
+import com.vaadin.flow.component.notification.Notification
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout
 import com.vaadin.flow.component.page.BodySize
 import com.vaadin.flow.component.page.Viewport
-import com.vaadin.flow.component.textfield.TextField
-import com.vaadin.flow.data.value.ValueChangeMode
 import com.vaadin.flow.router.Route
+import com.vaadin.flow.router.RouterLayout
+import com.vaadin.flow.router.RouterLink
+import com.vaadin.flow.spring.annotation.SpringComponent
+import com.vaadin.flow.spring.annotation.UIScope
 import com.vaadin.flow.theme.Theme
-import com.vaadin.flow.theme.lumo.Lumo
 import com.vaadin.flow.theme.material.Material
-import groovy.util.logging.Slf4j
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.Banner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.context.ApplicationContext
+
+import javax.annotation.PostConstruct
+import java.lang.annotation.*
+import java.util.function.Consumer
+import java.util.function.Function
 
 @SpringBootApplication
 class Application extends SpringBootServletInitializer {
     static void main(String[] args) throws Exception {
-        new SpringApplication(Application).tap{
+        new SpringApplication(Application).tap {
             // further config...
             bannerMode = Banner.Mode.OFF
             run(args)
@@ -34,38 +40,169 @@ class Application extends SpringBootServletInitializer {
     }
 }
 
-@RestController
-class HelloWorldController {
-    @GetMapping("/hello")
-    String index() {
-        return "Hello World"
+// Annotations
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@interface Spot {
+    int order() default 0;
+
+    // DECIDE: class based?  name based?
+    Class<? extends SpotContainer> container();
+
+    Class<? extends SpotComponentFactory> componentFactory();
+}
+
+interface SpotContainer extends Consumer<Component> {}
+
+interface SpotComponentFactory extends Function<Object, Component> {}
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@interface Translatable {
+    String i18nKey();
+
+    String i18nDefault() default ""; // FIXME: can not set default null?
+
+    // TODO: can args even be handled here?
+}
+
+// Spot Containers
+
+@SpringComponent
+@UIScope
+class MainMenu extends Div implements SpotContainer {
+
+    private final ApplicationContext ctx
+
+    @Autowired
+    MainMenu(ApplicationContext ctx) {
+        this.ctx = ctx
+        element.themeList.add('dark')
+    }
+
+    @PostConstruct
+    void init() {
+        // FIXME: move into tooling
+        ctx.getBeansWithAnnotation(Spot).collect { k, v ->
+            [v.getClass().getAnnotation(Spot), v]
+        }.findAll { i, v ->
+            i.container() == MainMenu
+        }.sort { // can't destructure here
+            it[0].order()
+        }.each { i, v ->
+            accept(ctx.getBean(i.componentFactory()).apply(v))
+        }
+    }
+
+    @Override
+    void accept(Component component) {
+        add(new Div(component))
     }
 }
 
-@Route('')
-@Slf4j
+// Spot Component Factories
+
+@SpringComponent
+class RouterLinkSpotComponentFactory implements SpotComponentFactory {
+
+    private final TranslationService translationService
+    @Autowired RouterLinkSpotComponentFactory(TranslationService translationService) { this.translationService = translationService }
+
+    @Override
+    Component apply(Object o) {
+        return new RouterLink(translationService.t(o.getClass().getAnnotation(Translatable)), o.getClass())
+    }
+}
+
+@SpringComponent
+class ButtonSpotComponentFactory implements SpotComponentFactory {
+
+    private final TranslationService translationService
+    @Autowired
+    ButtonSpotComponentFactory(TranslationService translationService) { this.translationService = translationService }
+
+    @Override
+    Component apply(Object o) {
+        new Button(translationService.t(o.getClass().getAnnotation(Translatable)), { (o as Runnable).run() })
+    }
+}
+
+@SpringComponent
+class TranslationService {
+    String t(Translatable t) {
+        t?.i18nDefault() ?: "T: ${t?.i18nKey()}"
+    }
+}
+
+// EXAMPLES
+
+@SpringComponent
+@UIScope
 @Theme(Material)
 @HtmlImport('frontend:///styles.html')
 @BodySize(height = "100vh", width = "100vw")
 @Viewport("width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes")
-class MainLayout extends Composite<VerticalLayout> {
-    MainLayout() {
-        H1 label
-        TextField input
-        content.add(
-                label = new H1(),
-                input = new TextField("Enter name to greet...").tap {
-                    addValueChangeListener({
-                        label.text = "Hello ${it.value ?: "World"}"
-                    } as HasValue.ValueChangeListener)
-                    valueChangeMode = ValueChangeMode.EAGER
-                    value = "Flow"
-                    focus()
-                },
-                new Button("Greet the server log", {
-                    log.info "Hello ${input.value ?: "World"} to the server log"
-                }),
-        )
+class MainLayout extends HorizontalLayout implements RouterLayout {
+
+    private MainMenu mainMenu
+
+    @Autowired
+    MainLayout(MainMenu mainMenu) {
+        this.mainMenu = mainMenu
+    }
+
+    @PostConstruct
+    void init() {
+        add(mainMenu)
     }
 }
 
+@Route(value = '', layout = MainLayout)
+@SpringComponent
+@UIScope
+@Spot(container = MainMenu, componentFactory = RouterLinkSpotComponentFactory)
+@Translatable(i18nKey = 'i18n.home', i18nDefault = 'Home')
+class HomeView extends Div {
+    @PostConstruct
+    void init() {
+        add(new H1("Home"), new Div(new Span("This is a test view")))
+    }
+}
+
+@Route(value = 'x1', layout = MainLayout)
+@SpringComponent
+@UIScope
+@Spot(container = MainMenu, componentFactory = RouterLinkSpotComponentFactory)
+@Translatable(i18nKey = 'i18n.x1', i18nDefault = 'X1')
+class X1View extends Div {
+    @PostConstruct
+    void init() {
+        add(new H1("X1"), new Div(new Span("This is a test view")))
+    }
+}
+
+@Route(value = 'x2', layout = MainLayout)
+@SpringComponent
+@UIScope
+@Spot(container = MainMenu, componentFactory = RouterLinkSpotComponentFactory)
+@Translatable(i18nKey = 'i28n.x2', i18nDefault = 'X2')
+class X2View extends Div {
+    @PostConstruct
+    void init() {
+        add(new H1("X2"), new Div(new Span("This is a test view")))
+    }
+}
+
+@SpringComponent
+@UIScope
+@Spot(container = MainMenu, componentFactory = ButtonSpotComponentFactory, order = 1000)
+@Translatable(i18nKey = 'i28n.doc', i18nDefault = 'Doc')
+class DocCommand implements Runnable {
+    @Override
+    void run() {
+        Notification.show("Doc")
+    }
+}
